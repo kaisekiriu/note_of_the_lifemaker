7275395a1c0c56d8dd8aab628a56a331d89717df
kaisekiriu <kaisekiriu@gmail.com>
2024-02-22 12:35:53 +0900

    Quartz sync: Feb 22, 2024, 12:35 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -83,6 +83,7 @@ const config: QuartzConfig = {
       Plugin.ContentIndex({
         enableSiteMap: true,
         enableRSS: true,
+        includeEmptyFiles: true, // added
       }),
       Plugin.Assets(),
       Plugin.Static(),

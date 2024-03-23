9a967e00d217014adff6667699e3f195ac57752d
kaisekiriu <kaisekiriu@gmail.com>
2024-03-23 16:10:16 +0900

    Quartz sync: Mar 23, 2024, 4:10 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -26,14 +26,16 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
+          // light: "#faf8f8",
+          light: "#fafafa",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
           dark: "#2b2b2b",
           secondary: "#284b63",
           tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
+          // highlight: "rgba(143, 159, 169, 0.15)",
+          highlight: "rgba(143, 159, 169, 0.00)",
         },
         darkMode: {
           light: "#161618",
@@ -43,7 +45,8 @@ const config: QuartzConfig = {
           dark: "#ebebec",
           secondary: "#7b97aa",
           tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
+          // highlight: "rgba(143, 159, 169, 0.15)",
+          highlight: "rgba(143, 159, 169, 0.00)",
         },
       },
     },
